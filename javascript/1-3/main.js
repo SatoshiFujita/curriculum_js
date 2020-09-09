9a94be121ea87388be0acf59aea821ce6038c311
SatoshiFujita <70784951+SatoshiFujita@users.noreply.github.com>
2020-09-09 12:49:09 +0900

    課題提出1-3
    問１〜問３までの課題

---
+++ javascript/1-3/main.js
@@ -0,0 +1,29 @@
+//問１
+
+let a
+ console.log(a != 10);
+
+
+let b
+if (b >= 10 && b < 20 && b % 2 == 0) {
+ console.log(b);
+}
+//問２
+
+let x = 10;
+if (x >=10 && x <= 20){
+    console.log("成功です");
+}else{
+    console.log("失敗です");
+}
+
+//問３
+let y = 0;
+//偶数の条件式
+if(y % 2 ===0){
+    console.log("偶数です");
+}
+//奇数の条件式
+if (y% 2!==0){
+    console.log("奇数です");
+}
